86e6ebb834f38385e800cbfe3f9037fd67837c09
john3hunnid10 <johnadamsnyc@gmail.com>
2024-03-05 01:12:39 -0500

    Need to add an if statement

--- mastermind.c
+++ mastermind.c
@@ -17,20 +17,28 @@ void main(){
     }
     //running the game
     while(rounds<10){
-        char playerGuess[4];
+        char playerGuess[5];
         char printStatement[4];
         printf("Enter your 4-button sequence out of: X, B, A, Y, L, R");
         scanf("%s", playerGuess);
         while(isValidGuess(playerGuess)==1){
         printf("Please enter a proper guess");
-           scanf("%s", playerGuess);
+            scanf("%s", playerGuess);
+            int c;
+            while ((c=getchar())!='\n'&& c!=EOF);
         }
         rounds++;
     }
 
 }
 int isValidGuess(const str *guess){
-    
+    for (int i=0; i<len; i++){
+        if(strchr(guess+i+1, guess[i]) != NULL)
+            return 0; 
+        if(guess[i]!='X'&&guess[i]!='R'&&guess[i]!='A'&&guess[i]!='B'&&guess[i]!='L'&&guess[i]!='Y')
+            return 0;
+    }
+    return 1;
 }
 int isInArray(char array[], int size, char value) {
     if(size==1){
